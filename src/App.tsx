071ff4e0c626729a3ef2faf6060ec27fbc604d96
Gabriel Badaró <grbadas@gmail.com>
2023-05-10 23:05:10 -0300

    curso

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ExpenseItem />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,18 @@
+import StyleItem from "./ExpenseItemStyle";
+
+ export default function ExpenseItem () {
+
+    return (
+        <>
+        <StyleItem>
+            <div className="expense-item">
+                March 28th 2021
+            </div>
+            <h2>Car Insurance</h2>
+            <div>$294.67</div>
+        </StyleItem>
+        </>
+    )
+ }
+
+ 
