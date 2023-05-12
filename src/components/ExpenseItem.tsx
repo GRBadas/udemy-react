eeabaac3c09b54f1fdd7eed1e8f2afd90e1f5d46
Gabriel Badaró <grbadas@gmail.com>
2023-05-11 23:37:30 -0300

    style divs

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -5,11 +5,13 @@ import StyleItem from "./ExpenseItemStyle";
     return (
         <>
         <StyleItem>
-            <div className="expense-item">
-                March 28th 2021
+        <div className="expense-item">
+            <div> March 28th 2021 </div> 
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
             </div>
-            <h2>Car Insurance</h2>
-            <div>$294.67</div>
+        </div>
         </StyleItem>
         </>
     )
