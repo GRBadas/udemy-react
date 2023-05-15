57208179f0b2173ce72aeed63aec73b41e39b846
Gabriel Badaró <grbadas@gmail.com>
2023-05-14 23:50:43 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -32,29 +32,14 @@ function App() {
 
   return (
     <>
-    <h2>Let's get started</h2>
-      <ExpenseItem 
-      name={expenses[0].name}
-      amount={expenses[0].amount}
-      date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      name={expenses[1].name}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      name={expenses[2].name}
-      amount={expenses[2].amount}
-      date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      name={expenses[3].name}
-      amount={expenses[3].amount}
-      date={expenses[3].date}
-      ></ExpenseItem>
+      <h2>Let's get started</h2>
+      <ExpenseItem expense={expenses[0]} />
+      <ExpenseItem expense={expenses[1]} />
+      <ExpenseItem expense={expenses[2]} />
+      <ExpenseItem expense={expenses[3]} />
     </>
-  )
+  );
+  
 }
 
 export default App
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,22 +1,19 @@
 import StyleItem from "./ExpenseItemStyle";
 import ExpenseDate from "./ExpenseDate";
 
- export default function ExpenseItem (props) {
-
-
+export default function ExpenseItem(props) {
     return (
-        <>
+      <>
         <StyleItem>
-        <div className="expense-item">
-        <ExpenseDate />
+          <div className="expense-item">
+            <ExpenseDate date={props.expense.date} />
             <div className="expense-item__description">
-                <h2>{props.name}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+              <h2>{props.expense.name}</h2>
+              <div className="expense-item__price">${props.expense.amount}</div>
             </div>
-        </div>
+          </div>
         </StyleItem>
-        </>
-    )
- }
-
- 
+      </>
+    );
+  }
+  
